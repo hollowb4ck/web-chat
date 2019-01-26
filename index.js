1bdb50a911e40d54d18c834473416cdff925b95c
Dima Myzulev <dmitriy.myzulev@seanetix.com>
2019-01-26 20:18:42 +0200

    add files

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.use('/scripts', express.static(`${__dirname}/node_modules/`));
+
+app.use('/styles', express.static(`${__dirname}/node_modules/`));
+
+app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+app.listen(port, () => {
+  console.log('server start:', port)
+});
